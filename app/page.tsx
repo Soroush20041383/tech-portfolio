bfa92c5712d34e93732b101295a6cba457ed1539
Soroush Osanlo <soroush.osanlo@gmail.com>
2025-02-13 10:47:33 -0500

    Fixed apostrophe issue in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
       <h1 className="text-5xl font-bold">🚀 Welcome to My Portfolio</h1>
-      <p className="mt-4 text-lg text-gray-400">I'm Soroush, a passionate software developer.</p>
+      <p className="mt-4 text-lg text-gray-400">I&apos;m Soroush, a passionate software developer.</p>
       <a
         href="https://github.com/Soroush20041383"
         target="_blank"
